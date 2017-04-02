3126cd1155e452a7f12eaa4393e8db7fe9713fc5
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-04-02 17:49:33 +0100

    Automated builds.

---
+++ build/paul/travis/2-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+INSTALL_DIR="${BUILD_PLATFORM}-${CMAKE_BUILD_TYPE}"
+BUILD_DIR="${INSTALL_DIR}-build"
+
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+cmake -G "${CMAKE_GENERATOR}" .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" ${CMAKE_OPTIONS}
+cmake --build . --config "${CMAKE_BUILD_TYPE}"
+cmake -DCMAKE_INSTALL_PREFIX=../"${INSTALL_DIR}" -P cmake_install.cmake
+cd ..
+find "${INSTALL_DIR}"
+
