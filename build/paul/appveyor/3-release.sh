4b12f6f5e198c1d74913709484958b47e7b3079a
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-04-04 20:55:47 +0100

    Automated builds.

---
+++ build/paul/appveyor/3-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+CMAKE_BUILD_TYPE="${CONFIGURATION}"
+INSTALL_DIR="${PROFILE}-${PLATFORM}-${CMAKE_BUILD_TYPE}"
+COMMIT_ID="${APPVEYOR_REPO_COMMIT}"
+
+7z a "${INSTALL_DIR}.zip" "${INSTALL_DIR}"
+
+github-release \
+  paulthomson/thrift \
+  "v-${COMMIT_ID}" \
+  "${COMMIT_ID}" \
+  "$(echo -e "Automated build.\n$(git log --graph -n 3 --abbrev-commit --pretty='format:%h - %s <%an>')")" \
+  "${INSTALL_DIR}.zip"
+
