c0573b455a8a27de9107274e13cb5e03b5da2cb6
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-04-04 20:55:47 +0100

    Automated builds.

---
+++ build/paul/appveyor/2-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+CMAKE_BUILD_TYPE="${CONFIGURATION}"
+INSTALL_DIR="${PROFILE}-${PLATFORM}-${CMAKE_BUILD_TYPE}"
+BUILD_DIR="$INSTALL_DIR-build"
+
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+cmake -G "${CMAKE_GENERATOR}" .. "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" ${CMAKE_OPTIONS}
+cmake --build . --config "${CMAKE_BUILD_TYPE}"
+cmake "-DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}" "-DBUILD_TYPE=${CMAKE_BUILD_TYPE}" -P cmake_install.cmake
+cd ..
+
